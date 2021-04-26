47cba2b2b105772c0120ca49ee884d1c49d0b237
FayxChance <fayxchancetom@outlook.com>
2021-04-26 15:40:57 +0200

    Test deploy 5

--- src/test/java/com/onehoeyman/battle/BattleApplicationTests.java
+++ src/test/java/com/onehoeyman/battle/BattleApplicationTests.java
@@ -3,10 +3,8 @@ package com.onehoeyman.battle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BattleApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
